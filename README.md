43fb2bc993bfd4ce0db1f1f50797ccd7ee5bcdce
unknown <ssk66221@gmail.com>
2025-07-04 17:30:29 +0545

    feat(setup): project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# ProjectHive: College Project Archive Platform
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Hello sir how are you
+      <Button className='bg-red-500' > Project Hive </Button>
+    </div>
+  )
+}
+
+export default page
