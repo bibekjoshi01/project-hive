73b0d805de5a5893107313e677cd2b9b1325f397
Bibek Joshi <bibekjoshi34@gmail.com>
2025-08-09 11:50:46 +0545

    fix: snackbar issue

--- src/lib/SnackBarProvider.tsx
+++ src/lib/SnackBarProvider.tsx
@@ -1,6 +1,18 @@
 'use client';
 
-import { SnackbarProvider } from 'notistack';
+import { setSnackbar } from '@/utils/notifier';
+import { SnackbarProvider, useSnackbar } from 'notistack';
+import { useEffect } from 'react';
+
+export function SnackbarInitializer() {
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    setSnackbar(enqueueSnackbar);
+  }, [enqueueSnackbar]);
+
+  return null; // no UI needed
+}
 
 export default function ClientLayoutWrapper({
   children,
@@ -13,6 +25,7 @@ export default function ClientLayoutWrapper({
       anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
       autoHideDuration={3000}
     >
+      <SnackbarInitializer />
       {children}
     </SnackbarProvider>
   );
